fcfe89ab5d506f9bd86a1ba4bf862fdf6d91e1dc
Kintali Naveesh <coolnavesh@gmail.com>
2019-09-08 20:28:00 +0530

    cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,6 @@
   <div>
     <Header />
     <Footer />
-    <!-- <div class="chat-notification">
-      <div class="chat-notification-logo-wrapper">
-        <img
-          class="chat-notification-logo"
-          src="/logo.svg"
-          alt="ChitChat Logo"
-        >
-      </div>
-      <div class="chat-notification-content">
-        <h4 class="chat-notification-title">ChitChat</h4>
-        <p class="chat-notification-message">You have a new message!</p>
-      </div>
-    </div> -->
   </div>
 </template>
 <script>
