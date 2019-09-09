43afacd3c00f2bf0c228889ad43e2fd73c08d228
Swadesh Behera <itswadesh@gmail.com>
2019-09-09 15:46:39 +0530

    fix: Product alignment

--- components/LeftSideBar.vue
+++ components/LeftSideBar.vue
@@ -1,52 +1,60 @@
 <template>
-    <div class="flex flex-wrap">
-        <div class="w-1/6 margin-right  flex">
-            <div class="bb w-screen">
-                <ul class="ml-2 py-2 font-semibold text-sm px-3">
-                    <input  type="radio" name="gender"><li><a   href="#">Women</a></li>
-                    <input   type="radio" name="gender"><li><a   href="#">Girls</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="w-5/6 right-body margin-right flex p-2">
-            <!-- CARD COMPONENT start -->
-            <div class="bg-white">
-                <div>
-                    <img src="/cardimage.webp" alt="" class="h-3/5 w-3/5" />
-                </div>
-                <div class="ml-1 pt-1">
-                    <p class="text-xl text-gray-900 font-bold ">Dorothy Perkins</p>
-                    <p class=" text-black font-thin">Women solid wrap dress</p>
-                    <p class=" text-gray-600">Rs.2990</p>
-                </div>
-            </div>
-            <!-- CARD COMPONENT end -->
-            
-        </div>
+  <div class="flex flex-wrap">
+    <div class="w-1/6 margin-right  flex">
+      <div class="bb w-screen">
+        <ul class="ml-2 py-2 font-semibold text-sm px-3">
+          <input
+            type="radio"
+            name="gender"
+          >
+          <li><a href="#">Women</a></li>
+          <input
+            type="radio"
+            name="gender"
+          >
+          <li><a href="#">Girls</a></li>
+        </ul>
+      </div>
     </div>
+    <div class="w-5/6 right-body margin-right flex flex-row flex-wrap p-2 items-center">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import Product from "~/components/Product";
 
-}
+export default {
+  components: { Product }
+};
 </script>
 
 <style scoped>
-input{
-    float: left;
-    margin-top: .5em;
-    margin-right: .5em;
+input {
+  float: left;
+  margin-top: 0.5em;
+  margin-right: 0.5em;
 }
-.bb{
-    border-bottom: 1px solid lightgray;
-    width:100%;
+.bb {
+  border-bottom: 1px solid lightgray;
+  width: 100%;
 }
-.margin-right{
-    border-right: 1px solid lightgray;
-    border-top: 1px solid lightgray;
+.margin-right {
+  border-right: 1px solid lightgray;
+  border-top: 1px solid lightgray;
 }
-.right-body{
-    padding: 1em;
+.right-body {
+  padding: 1em;
 }
 </style>
---
+++ components/Product.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-white">
+    <div>
+      <img
+        src="/cardimage.webp"
+        alt=""
+        class="h-3/5 w-3/5"
+      />
+    </div>
+    <div class="ml-1 pt-1">
+      <p class="text-xl text-gray-900 font-bold ">Dorothy Perkins</p>
+      <p class=" text-black font-thin">Women solid wrap dress</p>
+      <p class=" text-gray-600">Rs.2990</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
