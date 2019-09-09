9f52fce31362793d4a5f8fde569827b74d92464d
Kintali Naveesh <coolnavesh@gmail.com>
2019-09-09 18:35:50 +0530

    final update from my side

--- components/LeftSideBar.vue
+++ components/LeftSideBar.vue
@@ -1,18 +1,176 @@
 <template>
   <div class="flex flex-wrap">
-    <div class="w-1/6 margin-right  flex">
-      <div class="bb w-screen">
+    <div class="w-1/6 margin-right bb flex flex-row flex-wrap">
+      <div class="bb w-screen ">
         <ul class="ml-2 py-2 font-semibold text-sm px-3">
           <input
             type="radio"
             name="gender"
           >
-          <li><a href="#">Women</a></li>
+          <li><a  href="#">Women</a></li>
           <input
             type="radio"
             name="gender"
           >
           <li><a href="#">Girls</a></li>
+        </ul>
+      </div>
+      <div class="bb w-screen ">
+        <p class="ml-2 py-2 font-semibold text-sm px-3">BRAND</p>
+        <ul class="ml-2 py-2 font-semibold text-gray-600 text-sm px-3">
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">FOREVER 21(1288)</a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">DOROTHY PERKINS(1211)</a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">Peppermint(943)</a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">MANGO(779)</a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">next(644)</a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">H&M(585)</a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">Tokyo Talkies(580)</a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">Vero Moda(505)</a></li>
+        </ul>
+      </div>
+
+
+      <div class="bb w-screen ">
+        <p class="ml-2 py-2 font-semibold text-sm px-3">PRICE</p>
+        <ul class="ml-2 py-2 text-gray-600 font-semibold text-sm px-3">
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">RS. 184 to Rs.4638 <span>(30807)</span></a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">RS. 184 to Rs.4638 <span>(30807)</span></a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">RS. 184 to Rs.4638 <span>(30807)</span></a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">RS. 184 to Rs.4638 <span>(30807)</span></a></li>
+        </ul>
+      </div>
+
+      <div class="bb w-screen ">
+        <p class="ml-2 py-2 font-semibold text-sm px-3">COLOR</p>
+        <ul class="ml-2 py-2 text-gray-600 font-semibold text-sm px-3">
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">Black <span>(30807)</span></a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">Navy Blue <span>(30807)</span></a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">Blue <span>(30807)</span></a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">White<span>(30807)</span></a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">Pink<span>(30807)</span></a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">Red<span>(30807)</span></a></li>
+          <input
+            type="checkbox"
+            name="gender"
+          >
+          <li><a class="text-xs" href="#">Green<span>(30807)</span></a></li>
+        </ul>
+      </div>
+      <div class="bb w-screen ">
+        <p class="ml-2 py-2 font-semibold text-sm px-3">DISCOUNT RANGE</p>
+        <ul class="ml-2 py-2 text-gray-600 font-semibold text-sm px-3">
+          <input
+            type="radio"
+            name="discount"
+          >
+          <li><a  href="#">10% and above</a></li>
+          <input
+            type="radio"
+            name="discount"
+          >
+          <li><a href="#">20% and above</a></li>
+          <input
+            type="radio"
+            name="discount"
+          >
+          <li><a href="#">30% and above</a></li>
+          <input
+            type="radio"
+            name="discount"
+          >
+          <li><a href="#">40% and above</a></li>
+          <input
+            type="radio"
+            name="discount"
+          >
+          <li><a href="#">50% and above</a></li>
+          <input
+            type="radio"
+            name="discount"
+          >
+          <li><a href="#">60% and above</a></li>
+          <input
+            type="radio"
+            name="discount"
+          >
+          <li><a href="#">70% and above</a></li>
+          <input
+            type="radio"
+            name="discount"
+          >
+          <li><a href="#">80% and above</a></li>
         </ul>
       </div>
     </div>
@@ -28,7 +186,12 @@
       <Product />
       <Product />
       <Product />
+      <Product />
+      <div class=" flex flex-row w-full flex-wrap p-2">
+        <ul class="pagination-container w-full"><li class="pagination-paginationMeta"><!-- react-text: 2463 -->Page <!-- /react-text --><!-- react-text: 2464 -->1<!-- /react-text --><!-- react-text: 2465 --> of <!-- /react-text --><!-- react-text: 2466 -->627<!-- /react-text --></li><li class="pagination-active"><a href="https://www.myntra.com/clothing-dresses?sort=new"><!-- react-text: 2469 -->1<!-- /react-text --></a></li><li class="pagination-number"><a href="https://www.myntra.com/clothing-dresses?sort=new&amp;p=2"><!-- react-text: 2472 -->2<!-- /react-text --></a></li><li class="pagination-number"><a href="https://www.myntra.com/clothing-dresses?sort=new&amp;p=3"><!-- react-text: 2475 -->3<!-- /react-text --></a></li><li class="pagination-number"><a href="https://www.myntra.com/clothing-dresses?sort=new&amp;p=4"><!-- react-text: 2478 -->4<!-- /react-text --></a></li><li class="pagination-number"><a href="https://www.myntra.com/clothing-dresses?sort=new&amp;p=5"><!-- react-text: 2481 -->5<!-- /react-text --></a></li><li class="pagination-number"><a href="https://www.myntra.com/clothing-dresses?sort=new&amp;p=6"><!-- react-text: 2484 -->6<!-- /react-text --></a></li><li class="pagination-number"><a href="https://www.myntra.com/clothing-dresses?sort=new&amp;p=7"><!-- react-text: 2487 -->7<!-- /react-text --></a></li><li class="pagination-number"><a href="https://www.myntra.com/clothing-dresses?sort=new&amp;p=8"><!-- react-text: 2490 -->8<!-- /react-text --></a></li><li class="pagination-number"><a href="https://www.myntra.com/clothing-dresses?sort=new&amp;p=9"><!-- react-text: 2493 -->9<!-- /react-text --></a></li><li class="pagination-number"><a href="https://www.myntra.com/clothing-dresses?sort=new&amp;p=10"><!-- react-text: 2496 -->10<!-- /react-text --></a></li><li class="pagination-next"><a rel="next" href=""><!-- react-text: 2499 -->Next<!-- /react-text --><span class="pagination-arrowRight"><img src="/right.png" class="arrow"></span></a></li></ul>
+      </div>
     </div>
+    
   </div>
 </template>
 
@@ -41,20 +204,85 @@ export default {
 </script>
 
 <style scoped>
+.arrow{
+  position: relative;
+    right: -40px;
+    top: -16px;
+    width: 0.6em;
+}
 input {
   float: left;
-  margin-top: 0.5em;
   margin-right: 0.5em;
 }
 .bb {
   border-bottom: 1px solid lightgray;
-  width: 100%;
+  height: max-content;
 }
 .margin-right {
   border-right: 1px solid lightgray;
   border-top: 1px solid lightgray;
+  height: unset;
 }
 .right-body {
   padding: 1em;
+}
+.results-showMoreContainer {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+.pagination-container {
+    list-style: none;
+    text-align: center;
+    border-top: 1px solid #EDEBEF;
+    padding-top: 24px;
+    padding-bottom: 60px;
+    color: #282c3f;
+}
+.pagination-container .pagination-paginationMeta {
+    color: #535766;
+    font-size: 14px;
+    float: left;
+    margin-top: 6px;
+    margin-right: 10px;
+    cursor: default;
+    font-weight: 400;
+}
+.pagination-container .pagination-active {
+    font-weight: 500;
+    color: #fff;
+    letter-spacing: 0.4px;
+    -webkit-border-radius: 2px;
+    -moz-border-radius: 2px;
+    border-radius: 2px;
+    background-color: #282c3f;
+}
+.pagination-container li {
+    display: inline-block;
+    margin-right: 9px;
+    font-weight: 500;
+    cursor: pointer;
+    padding: 6px 8px;
+    -webkit-border-radius: 2px;
+    -moz-border-radius: 2px;
+    border-radius: 2px;
+}
+.pagination-container {
+    list-style: none;
+    text-align: center;
+    border-top: 1px solid #EDEBEF;
+    padding-top: 24px;
+    padding-bottom: 60px;
+    color: #282c3f;
+}
+.pagination-container .pagination-next {
+    height: 42px;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    border-radius: 4px;
+    border: 1px solid #d4d5d9;
+    text-align: center;
+    margin-left: 64px;
+    padding: 10px 22px;
 }
 </style>
--- components/Product.vue
+++ components/Product.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="bg-white">
-    <div>
+  <div class="bg-white w-60 ml-5 mr-6 mb-10">
+    <div class="w-60">
       <img
         src="/cardimage.webp"
         alt=""
-        class="h-3/5 w-3/5"
+        class="h-64 w-40%"
       />
     </div>
     <div class="ml-1 pt-1">
       <p class="text-xl text-gray-900 font-bold ">Dorothy Perkins</p>
-      <p class=" text-black font-thin">Women solid wrap dress</p>
+      <p class=" text-black font-thin">Women solid wrap</p>
       <p class=" text-gray-600">Rs.2990</p>
     </div>
   </div>
+
+  
 </template>
 
 <script>
