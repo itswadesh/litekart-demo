5acb107f19a1d06ab2a5339d6024f29e3d80b84b
itswadesh <itswadesh@gmail.com>
2019-09-15 10:07:03 +0530

    converted to spa

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,25 +3,19 @@ const { API_URL, head } = require("./config");
 const PROXY = API_URL
 
 export default {
-  mode: 'universal',
+  mode: 'spa',
   head,
   loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
+  css: [],
   plugins: [
     '~/plugins/filters.js',
     { src: '~/plugins/init.js', ssr: false },
     { src: '~/plugins/lazy.js', ssr: false }
   ],
   buildModules: [
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
   ],
   modules: [
-    // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/font-awesome',
     '@nuxtjs/pwa',
