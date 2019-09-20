747c280d94db768baed5ffc5edda55293ece5df6
Kintali Naveesh <coolnavesh@gmail.com>
2019-09-20 20:32:49 +0530

    toast code

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -9,15 +9,12 @@
             :key="ix"
             class="w-24 cursor-pointer"
             v-lazy="i"
-            alt=""
+            alt
             @click="showAsCurrentImage(i)"
           />
         </div>
         <div class="flex-1 xs:order-1 md:order-2">
-          <img
-            class="inline-block w-full border border-gray-400"
-            v-lazy="`${currentImage}`"
-          />
+          <img class="inline-block w-full border border-gray-400" v-lazy="`${currentImage}`" />
           <div class="w-full flex">
             <button
               :disabled="!selectedVariant.price || selectedVariant.stock==0 || $store.state.loading"
@@ -25,16 +22,12 @@
               @click="addToBag({pid:product._id, vid:selectedVariant._id,qty:1})"
               class="bg-orange-500 flex-1 my-3 p-3 md:p-5 mr-2 text-center font-bold text-white focus:outline-none text-xs lg:text-lg"
             >
-              <i
-                class="fa fa-shopping-cart mr-2"
-                aria-hidden="true"
-              ></i> ADD TO CART
+              <i class="fa fa-shopping-cart mr-2" aria-hidden="true"></i> ADD TO CART
             </button>
-            <button class="bg-yellow-600 flex-1 my-3 p-3 md:p-5 text-center font-bold text-white focus:outline-none text-xs lg:text-lg">
-              <i
-                class="fa fa-bolt mr-2"
-                aria-hidden="true"
-              ></i>BUY NOW
+            <button
+              class="bg-yellow-600 flex-1 my-3 p-3 md:p-5 text-center font-bold text-white focus:outline-none text-xs lg:text-lg"
+            >
+              <i class="fa fa-bolt mr-2" aria-hidden="true"></i>BUY NOW
             </button>
           </div>
         </div>
@@ -43,67 +36,43 @@
         <div class="w-full flex flex-wrap">
           <p class="w-9/12 left-0 text-left text-xs text-gray-500">
             Home
-            <i
-              class="fa fa-angle-right"
-              aria-hidden="true"
-            ></i>
+            <i class="fa fa-angle-right" aria-hidden="true"></i>
             Wearable Smart Watch
-            <i
-              class="fa fa-angle-right"
-              aria-hidden="true"
-            ></i> Smart Bands
-            <i
-              class="fa fa-angle-right"
-              aria-hidden="true"
-            ></i> {{product.name}}
+            <i class="fa fa-angle-right" aria-hidden="true"></i> Smart Bands
+            <i class="fa fa-angle-right" aria-hidden="true"></i>
+            {{product.name}}
           </p>
           <p class="w-3/12 text-right right-0 pr-3">
-            <i
-              class="fa fa-share pr-2 text-gray-500"
-              aria-hidden="true"
-            ></i>Share
+            <i class="fa fa-share pr-2 text-gray-500" aria-hidden="true"></i>Share
           </p>
         </div>
         <div class="w-full">
           <p class="xs:w-full lg:w-9/12 left-0 text-left text-lg text-black">{{product.name}}</p>
           <p class="mt-2">
-            <a
-              href
-              class="w-9/12 left-0 text-left text-lg text-black mt-2"
-            >
+            <a href class="w-9/12 left-0 text-left text-lg text-black mt-2">
               <span class="text-white text-xs p-1 bg-green-700 rounded">
                 4.1
-                <i
-                  class="fa fa-star"
-                  aria-hidden="true"
-                ></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
               </span>
               <span class="text-gray-500 text-sm p-1">214 Ratings & 22 Reviews</span>
             </a>
           </p>
-          <p class="w-9/12 left-0 text-left text-2xl font-bold text-black mt-2">{{product.variants[0].price | currency}}</p>
+          <p
+            class="w-9/12 left-0 text-left text-2xl font-bold text-black mt-2"
+          >{{product.variants[0].price | currency}}</p>
           <ul class="mt-2 text-sm">
             <li class="mt-3">
-              <i
-                class="mr-2 fa fa-calendar-check-o text-green-500"
-                aria-hidden="true"
-              ></i>No cost EMI ₹750/month. Standard EMI also available
+              <i class="mr-2 fa fa-calendar-check-o text-green-500" aria-hidden="true"></i>No cost EMI ₹750/month. Standard EMI also available
               <a
                 href
                 class="text-blue-500 font-semibold"
               >
                 View Plans
-                <i
-                  class="fa fa-angle-right"
-                  aria-hidden="true"
-                ></i>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
               </a>
             </li>
             <li class="mt-3">
-              <i
-                class="mr-2 fa fa-tag text-green-500"
-                aria-hidden="true"
-              ></i>
+              <i class="mr-2 fa fa-tag text-green-500" aria-hidden="true"></i>
               <b>Bank Offer</b> 5% Unlimited Cashback on Flipkart Axis Bank Credit Card
               <a
                 href
@@ -111,10 +80,7 @@
               >T & C</a>
             </li>
             <li class="mt-3">
-              <i
-                class="mr-2 fa fa-tag text-green-500"
-                aria-hidden="true"
-              ></i>
+              <i class="mr-2 fa fa-tag text-green-500" aria-hidden="true"></i>
               <b>Bank Offer</b> 5% Cashback* on HDFC Bank Debit Cards
               <a
                 href
@@ -122,10 +88,7 @@
               >T & C</a>
             </li>
             <li class="mt-3">
-              <i
-                class="mr-2 fa fa-tag text-green-500"
-                aria-hidden="true"
-              ></i>
+              <i class="mr-2 fa fa-tag text-green-500" aria-hidden="true"></i>
               <b>Bank Offer</b> Extra 5% off* with Axis Bank Buzz Credit Card
               <a
                 href
@@ -137,16 +100,15 @@
         <div class="w-full flex flex-wrap mt-3">
           <p class="w-20 pt-2 text-left text-sm text-gray-500">Delivery</p>
           <div class="lg:w-1/3 xs:w-full text-left left-0 relative">
-            <i
-              aria-hidden="true"
-              class="fa fa-map-marker absolute mt-3"
-            ></i>
+            <i aria-hidden="true" class="fa fa-map-marker absolute mt-3"></i>
             <input
               type="search"
               placeholder="Enter Delivery Pincode"
               class="p-2 pl-5 w-full focus:outline-none text-sm border-blue-600 border-b"
             />
-            <button class="absolute text-sm mt-2 text-blue-500 font-bold focus:outline-none hover:text-gray-600 cursor-pointer">Check</button>
+            <button
+              class="absolute text-sm mt-2 text-blue-500 font-bold focus:outline-none hover:text-gray-600 cursor-pointer"
+            >Check</button>
             <br />
             <p class="w-full text-justify mt-2 text-sm text-red-500">Not a valid pincode</p>
           </div>
@@ -155,18 +117,9 @@
         <div class="w-full flex flex-wrap mt-3">
           <p class="w-20 pt-2 text-left text-sm text-gray-500">Color</p>
           <div class="xs:w-full lg:w-1/3 flex flex-wrap justify-start">
-            <img
-              class="w-14 h-12 cursor-pointer"
-              src="/small-product.png"
-            />
-            <img
-              class="w-14 h-12 cursor-pointer"
-              src="/small-product.png"
-            />
-            <img
-              class="w-14 h-12 cursor-pointer"
-              src="/small-product.png"
-            />
+            <img class="w-14 h-12 cursor-pointer" src="/small-product.png" />
+            <img class="w-14 h-12 cursor-pointer" src="/small-product.png" />
+            <img class="w-14 h-12 cursor-pointer" src="/small-product.png" />
           </div>
         </div>
 
@@ -177,7 +130,9 @@
             </div>
             <div class="w-48">
               <ul class="mt-2 text-sm">
-                <li class="mt-3">Activity Tracking, Sleep Tracking, Calories Burnt, Automatic Exercise Recognition</li>
+                <li
+                  class="mt-3"
+                >Activity Tracking, Sleep Tracking, Calories Burnt, Automatic Exercise Recognition</li>
                 <li class="mt-3">15 Goal Based Exercise Modes Like - Running, Biking, Yoga etc</li>
                 <li class="mt-3">24/7 Heart Rate Tracking</li>
                 <li class="mt-3">Swim-proof Water Resistant Upto 50 m</li>
@@ -191,22 +146,13 @@
             <div class="w-48">
               <ul class="mt-2 text-sm">
                 <li class="mt-3">
-                  <i
-                    class="mr-2 fa fa-shield"
-                    aria-hidden="true"
-                  ></i> 1 Year Warranty Provided by the Manufacturer from Date of Purchase
+                  <i class="mr-2 fa fa-shield" aria-hidden="true"></i> 1 Year Warranty Provided by the Manufacturer from Date of Purchase
                 </li>
                 <li class="mt-3">
-                  <i
-                    class="mr-2 fa fa-exchange"
-                    aria-hidden="true"
-                  ></i>24/7 Heart Rate Tracking
+                  <i class="mr-2 fa fa-exchange" aria-hidden="true"></i>24/7 Heart Rate Tracking
                 </li>
                 <li class="mt-3">
-                  <i
-                    class="mr-2 fa fa-inr"
-                    aria-hidden="true"
-                  ></i> 24/7 Heart Rate Tracking
+                  <i class="mr-2 fa fa-inr" aria-hidden="true"></i> 24/7 Heart Rate Tracking
                 </li>
               </ul>
             </div>
@@ -216,16 +162,10 @@
         <div class="w-full flex flex-wrap mt-3 mb-10 text-justify">
           <p class="w-20 pt-2 text-left text-sm text-gray-500">Seller</p>
           <div class="w-1/3 mt-3">
-            <a
-              href
-              class="text-blue-500"
-            >RetailNet</a>
+            <a href class="text-blue-500">RetailNet</a>
             <span class="text-white text-xs p-1 text-center bg-blue-500 rounded-l-lg rounded-r-lg">
               4.1
-              <i
-                class="fa fa-star"
-                aria-hidden="true"
-              ></i>
+              <i class="fa fa-star" aria-hidden="true"></i>
             </span>
           </div>
         </div>
@@ -240,10 +180,14 @@
         <div>
           <div class="w-full flex flex-wrap mt-3 border p-5">
             <p class="text-2xl">Product Description</p>
-            <p class="text-sm">Meet the Fitbit Inspire HR, your new partner who will guide you towards a healthier lifestyle. From tracking your heart rate and sleep quality to the number of calories you burn and the steps you take, the Inspire HR is designed to get you up and moving.</p>
+            <p
+              class="text-sm"
+            >Meet the Fitbit Inspire HR, your new partner who will guide you towards a healthier lifestyle. From tracking your heart rate and sleep quality to the number of calories you burn and the steps you take, the Inspire HR is designed to get you up and moving.</p>
           </div>
           <div class="w-full border-b border-l border-r p-5">
-            <p class="text-sm">Meet the Fitbit Inspire HR, your new partner who will guide you towards a healthier lifestyle. From tracking your heart rate and sleep quality to the number of calories you burn and the steps you take, the Inspire HR is designed to get you up and moving.</p>
+            <p
+              class="text-sm"
+            >Meet the Fitbit Inspire HR, your new partner who will guide you towards a healthier lifestyle. From tracking your heart rate and sleep quality to the number of calories you burn and the steps you take, the Inspire HR is designed to get you up and moving.</p>
           </div>
           <div class="w-full border-b border-l border-r p-5">
             <p class="text-md text-blue-500">
@@ -296,70 +240,65 @@
               <button class="bg-white border px-4 py-2 shadow font-normal">Rate Product</button>
             </p>
             <div class="text-sm w-full flex flex-wrap">
-              <div class="">
+              <div class>
                 <span class="text-black text-4xl text-center">4.1</span>
-                <i
-                  class="fa fa-star"
-                  aria-hidden="true"
-                ></i>
+                <i class="fa fa-star" aria-hidden="true"></i>
                 <br />
-                <span class="text-gray-500 text-xs p-1">214 Ratings <br />& 22 Reviews</span>
+                <span class="text-gray-500 text-xs p-1">
+                  214 Ratings
+                  <br />& 22 Reviews
+                </span>
               </div>
               <div class="flex-1">
                 <div class="w-full bg-grey-light">
                   <div class="flex flex-wrap">
                     <span>
                       5
-                      <i
-                        class="fa fa-star w-1/5"
-                        aria-hidden="true"
-                      ></i>
+                      <i class="fa fa-star w-1/5" aria-hidden="true"></i>
                     </span>
-                    <div class="w-4/5 h-2 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"></div>
+                    <div
+                      class="w-4/5 h-2 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"
+                    ></div>
                   </div>
 
                   <div class="flex flex-wrap">
                     <span>
                       4
-                      <i
-                        class="fa fa-star w-1/5"
-                        aria-hidden="true"
-                      ></i>
+                      <i class="fa fa-star w-1/5" aria-hidden="true"></i>
                     </span>
-                    <div class="w-4/5 h-2 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"></div>
+                    <div
+                      class="w-4/5 h-2 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"
+                    ></div>
                   </div>
 
                   <div class="flex flex-wrap">
                     <span>
                       3
-                      <i
-                        class="fa fa-star w-1/12"
-                        aria-hidden="true"
-                      ></i>
+                      <i class="fa fa-star w-1/12" aria-hidden="true"></i>
                     </span>
-                    <div class="w-4/5 h-2 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"></div>
+                    <div
+                      class="w-4/5 h-2 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"
+                    ></div>
                   </div>
 
                   <div class="flex flex-wrap">
                     <span>
                       2
-                      <i
-                        class="fa fa-star w-1/5"
-                        aria-hidden="true"
-                      ></i>
+                      <i class="fa fa-star w-1/5" aria-hidden="true"></i>
                     </span>
-                    <div class="w-4/5 h-2 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"></div>
+                    <div
+                      class="w-4/5 h-2 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"
+                    ></div>
                   </div>
 
                   <div class="flex flex-wrap">
                     <span>
                       1
-                      <i
-                        class="fa fa-star w-1/5"
-                        aria-hidden="true"
-                      ></i>
+                      <i class="fa fa-star w-1/5" aria-hidden="true"></i>
                     </span>
-                    <div class="w-4/5 h-2 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"></div>
+                    <div
+                      class="w-4/5 h-2 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"
+                    ></div>
                   </div>
                 </div>
               </div>
@@ -368,38 +307,25 @@
           <div class="w-full border-b border-l border-r p-5 flex flex-wrap">
             <span class="text-white text-xs p-1 bg-green-700 rounded">
               4.1
-              <i
-                class="fa fa-star"
-                aria-hidden="true"
-              ></i>
+              <i class="fa fa-star" aria-hidden="true"></i>
             </span>
             <p class="text-lg font-bold ml-3">Must Buy</p>
-            <div class="w-full text-xs pt-2">Quick Delivery by Flipkart. Many feature Activity Tracker at a reasonable Price Tag with an equally good App.</div>
+            <div
+              class="w-full text-xs pt-2"
+            >Quick Delivery by Flipkart. Many feature Activity Tracker at a reasonable Price Tag with an equally good App.</div>
             <div class="w-9/12 text-xs text-gray-500 pt-2">
               <span class="font-bold">B K Nanda</span>
-              <i
-                class="fa fa-certificate"
-                aria-hidden="true"
-              ></i>
+              <i class="fa fa-certificate" aria-hidden="true"></i>
               Certified Buyer, Jamshedpur
               5 months ago
             </div>
             <div class="w-3/12 text-xs text-gray-500 pt-2 text-sm">
               <p>
                 18
-                <i
-                  class="fa fa-thumbs-o-up mr-6"
-                  aria-hidden="true"
-                ></i>
+                <i class="fa fa-thumbs-o-up mr-6" aria-hidden="true"></i>
                 19
-                <i
-                  class="fa fa-thumbs-o-down mr-6"
-                  aria-hidden="true"
-                ></i>
-                <i
-                  class="fa fa-angle-down"
-                  aria-hidden="true"
-                ></i>
+                <i class="fa fa-thumbs-o-down mr-6" aria-hidden="true"></i>
+                <i class="fa fa-angle-down" aria-hidden="true"></i>
               </p>
             </div>
           </div>
@@ -557,7 +483,7 @@ export default {
         .show(
           `
       <div class="toast-card h-64">
-        <img class="img w-32 h-64" src="${this.currentImage}"/>
+        <img class="img w-24 h-48 object-cover" src="${this.currentImage}"/>
         <div class="detail">
           <div class="name">${this.product.name}</div>
           <div class="mute">Added to your cart</div>
@@ -572,7 +498,7 @@ export default {
             singleton: false
           }
         )
-        .goAway(5000);
+        .goAway(500000);
     },
     scrollTo() {
       window.scroll({
