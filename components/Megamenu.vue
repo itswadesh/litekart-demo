2e3117927780bdc1830cb930a549878dacbde04f
Kintali Naveesh <coolnavesh@gmail.com>
2019-09-20 10:42:29 +0530

    category highlight

--- components/Megamenu.vue
+++ components/Megamenu.vue
@@ -1,30 +1,22 @@
 <template>
   <ul class="flex">
-    <li
-      class="hoverable"
-      v-for="c in $store.state.categories"
-      :key="c._id"
-    >
+    <li class="hoverable hover:bg-gray-300" v-for="c in $store.state.categories" :key="c._id">
       <nuxt-link
         :to="`/search?q=${c.slug}`"
         class="relative block py-6 px-4 lg:p-6 text-sm lg:text-base font-bold"
       >
         {{c.name.toUpperCase()}}
-        <i
-          class="fa fa-angle-down"
-          aria-hidden="true"
-        ></i>
+        <i class="fa fa-angle-down" aria-hidden="true"></i>
       </nuxt-link>
       <div class="mega-menu mb-16 sm:mb-0 shadow-xl">
         <div class="mx-auto w-full flex flex-wrap justify-between mx-2">
-          <ul class="bg-gray-100 px-4 w-full sm:w-1/2 lg:w-1/5 border-gray-600 border-b lg:border-b-0 pb-6 pt-6 lg:pt-3">
+          <ul
+            class="bg-gray-100 px-4 w-full sm:w-1/2 lg:w-1/5 border-gray-600 border-b lg:border-b-0 pb-6 pt-6 lg:pt-3"
+          >
             <div class="flex items-center">
               <h3 class="font-bold text-sm text-black text-bold mb-2">
                 Mobiles
-                <i
-                  class="fa fa-caret-right"
-                  aria-hidden="true"
-                ></i>
+                <i class="fa fa-caret-right" aria-hidden="true"></i>
               </h3>
             </div>
             <div class="flex items-center py-3">
@@ -56,14 +48,13 @@
               </ul>
             </div>
           </ul>
-          <ul class="bg-gray-200 px-4 w-full sm:w-1/2 lg:w-1/5 border-gray-600 border-b lg:border-b-0 pb-6 pt-6 lg:pt-3">
+          <ul
+            class="bg-gray-200 px-4 w-full sm:w-1/2 lg:w-1/5 border-gray-600 border-b lg:border-b-0 pb-6 pt-6 lg:pt-3"
+          >
             <div class="flex items-center">
               <h3 class="font-bold text-sm text-black text-bold mb-2">
                 Mobile Accessories
-                <i
-                  class="fa fa-caret-right"
-                  aria-hidden="true"
-                ></i>
+                <i class="fa fa-caret-right" aria-hidden="true"></i>
               </h3>
             </div>
             <div class="flex items-center py-3">
@@ -95,14 +86,13 @@
               </ul>
             </div>
           </ul>
-          <ul class="bg-gray-100 px-4 w-full sm:w-1/2 lg:w-1/5 border-gray-600 border-b lg:border-b-0 pb-6 pt-6 lg:pt-3">
+          <ul
+            class="bg-gray-100 px-4 w-full sm:w-1/2 lg:w-1/5 border-gray-600 border-b lg:border-b-0 pb-6 pt-6 lg:pt-3"
+          >
             <div class="flex items-center">
               <h3 class="font-bold text-sm text-black text-bold mb-2">
                 Mobile Accessories
-                <i
-                  class="fa fa-caret-right"
-                  aria-hidden="true"
-                ></i>
+                <i class="fa fa-caret-right" aria-hidden="true"></i>
               </h3>
             </div>
             <div class="flex items-center py-3">
@@ -134,14 +124,13 @@
               </ul>
             </div>
           </ul>
-          <ul class="bg-gray-200 px-4 w-full sm:w-1/2 lg:w-1/5 border-gray-600 border-b lg:border-b-0 pb-6 pt-6 lg:pt-3">
+          <ul
+            class="bg-gray-200 px-4 w-full sm:w-1/2 lg:w-1/5 border-gray-600 border-b lg:border-b-0 pb-6 pt-6 lg:pt-3"
+          >
             <div class="flex items-center">
               <h3 class="font-bold text-sm text-black text-bold mb-2">
                 Mobile Accessories
-                <i
-                  class="fa fa-caret-right"
-                  aria-hidden="true"
-                ></i>
+                <i class="fa fa-caret-right" aria-hidden="true"></i>
               </h3>
             </div>
             <div class="flex items-center py-3">
@@ -173,14 +162,13 @@
               </ul>
             </div>
           </ul>
-          <ul class="bg-gray-100 px-4 w-full sm:w-1/2 lg:w-1/5 border-gray-600 border-b lg:border-b-0 pb-6 pt-6 lg:pt-3">
+          <ul
+            class="bg-gray-100 px-4 w-full sm:w-1/2 lg:w-1/5 border-gray-600 border-b lg:border-b-0 pb-6 pt-6 lg:pt-3"
+          >
             <div class="flex items-center">
               <h3 class="font-bold text-sm text-black text-bold mb-2">
                 Mobile Accessories
-                <i
-                  class="fa fa-caret-right"
-                  aria-hidden="true"
-                ></i>
+                <i class="fa fa-caret-right" aria-hidden="true"></i>
               </h3>
             </div>
             <div class="flex items-center py-3">
