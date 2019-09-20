72a4f49df3c2f8a0d374aec74a12b77015c843fb
Swadesh Behera <itswadesh@gmail.com>
2019-09-20 14:36:16 +0530

    fix: order-success

--- pages/order-success.vue
+++ pages/order-success.vue
@@ -3,9 +3,7 @@
     <div class="text-xl text-gray-700 font-bold p-5 border-b border-gray-200">Profile details</div>
     <div class="w-full text-sm text-center mt-5 mb-5 py-5 px-8">
       Just 2 step(s) to go and your profile's complete
-      <div
-        class="w-full h-1 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"
-      ></div>
+      <div class="w-full h-1 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"></div>
     </div>
 
     <div class="w-full">
@@ -13,11 +11,17 @@
         <a href="#">
           <li class="flex-wrap flex mt-5 mb-5 hover:bg-gray-200 py-5 px-8">
             <div class="w-1/12">
-              <i class="fa fa-id-card" aria-hidden="true"></i>
+              <i
+                class="fa fa-id-card"
+                aria-hidden="true"
+              ></i>
             </div>
             <div class="w-10/12">KOHN WICK</div>
             <div class="w-1/12">
-              <i class="fa fa-angle-right" aria-hidden="true"></i>
+              <i
+                class="fa fa-angle-right"
+                aria-hidden="true"
+              ></i>
             </div>
           </li>
         </a>
@@ -25,11 +29,17 @@
         <a href="#">
           <li class="flex-wrap flex mt-5 mb-5 hover:bg-gray-200 py-5 px-8">
             <div class="w-1/12">
-              <i class="fa fa-id-card" aria-hidden="true"></i>
+              <i
+                class="fa fa-id-card"
+                aria-hidden="true"
+              ></i>
             </div>
             <div class="w-10/12">9999999999</div>
             <div class="w-1/12">
-              <i class="fa fa-angle-right" aria-hidden="true"></i>
+              <i
+                class="fa fa-angle-right"
+                aria-hidden="true"
+              ></i>
             </div>
           </li>
         </a>
@@ -37,11 +47,17 @@
         <a href="#">
           <li class="flex-wrap flex mt-5 mb-5 hover:bg-gray-200 py-5 px-8">
             <div class="w-1/12">
-              <i class="fa fa-id-card" aria-hidden="true"></i>
+              <i
+                class="fa fa-id-card"
+                aria-hidden="true"
+              ></i>
             </div>
             <div class="w-10/12">kohnwick@gmail.com</div>
             <div class="w-1/12">
-              <i class="fa fa-angle-right" aria-hidden="true"></i>
+              <i
+                class="fa fa-angle-right"
+                aria-hidden="true"
+              ></i>
             </div>
           </li>
         </a>
@@ -49,11 +65,17 @@
         <a href="#">
           <li class="flex-wrap flex mt-5 mb-5 hover:bg-gray-200 py-5 px-8">
             <div class="w-1/12">
-              <i class="fa fa-id-card" aria-hidden="true"></i>
+              <i
+                class="fa fa-id-card"
+                aria-hidden="true"
+              ></i>
             </div>
             <div class="w-10/12">Password</div>
             <div class="w-1/12">
-              <i class="fa fa-angle-right" aria-hidden="true"></i>
+              <i
+                class="fa fa-angle-right"
+                aria-hidden="true"
+              ></i>
             </div>
           </li>
         </a>
@@ -62,8 +84,3 @@
   </div>
 </template>
 
-<script>
-export default {
-  layout: "account"
-};
-</script>
