ce34176d2a95e45294756ba16524f5445cb764b8
Kintali Naveesh <coolnavesh@gmail.com>
2019-09-20 14:31:46 +0530

    page issue

---
+++ pages/my/orderSuccess.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="lg:w-1/2 mt-10 xs:w-full bg-white shadow ml-10 py-5 px-8">
+    <div class="text-xl text-gray-700 font-bold p-5 border-b border-gray-200">Profile details</div>
+    <div class="w-full text-sm text-center mt-5 mb-5 py-5 px-8">
+      Just 2 step(s) to go and your profile's complete
+      <div
+        class="w-full h-1 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"
+      ></div>
+    </div>
+
+    <div class="w-full">
+      <ul class="w-full">
+        <a href="#">
+          <li class="flex-wrap flex mt-5 mb-5 hover:bg-gray-200 py-5 px-8">
+            <div class="w-1/12">
+              <i class="fa fa-id-card" aria-hidden="true"></i>
+            </div>
+            <div class="w-10/12">KOHN WICK</div>
+            <div class="w-1/12">
+              <i class="fa fa-angle-right" aria-hidden="true"></i>
+            </div>
+          </li>
+        </a>
+
+        <a href="#">
+          <li class="flex-wrap flex mt-5 mb-5 hover:bg-gray-200 py-5 px-8">
+            <div class="w-1/12">
+              <i class="fa fa-id-card" aria-hidden="true"></i>
+            </div>
+            <div class="w-10/12">9999999999</div>
+            <div class="w-1/12">
+              <i class="fa fa-angle-right" aria-hidden="true"></i>
+            </div>
+          </li>
+        </a>
+
+        <a href="#">
+          <li class="flex-wrap flex mt-5 mb-5 hover:bg-gray-200 py-5 px-8">
+            <div class="w-1/12">
+              <i class="fa fa-id-card" aria-hidden="true"></i>
+            </div>
+            <div class="w-10/12">kohnwick@gmail.com</div>
+            <div class="w-1/12">
+              <i class="fa fa-angle-right" aria-hidden="true"></i>
+            </div>
+          </li>
+        </a>
+
+        <a href="#">
+          <li class="flex-wrap flex mt-5 mb-5 hover:bg-gray-200 py-5 px-8">
+            <div class="w-1/12">
+              <i class="fa fa-id-card" aria-hidden="true"></i>
+            </div>
+            <div class="w-10/12">Password</div>
+            <div class="w-1/12">
+              <i class="fa fa-angle-right" aria-hidden="true"></i>
+            </div>
+          </li>
+        </a>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "account"
+};
+</script>
