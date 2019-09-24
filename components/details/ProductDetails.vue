8532a68865656fc5480ba29a577cbe25468098fc
Kintali Naveesh <coolnavesh@gmail.com>
2019-09-24 19:02:02 +0530

    product detail page as myntra

--- components/details/ProductDetails.vue
+++ components/details/ProductDetails.vue
@@ -1,6 +1,215 @@
 <template>
-  <div class="w-full md:w-6/12 lg:w-7/12 p-2 md:pl-6">
-    Details
+  <div class="w-full md:w-6/12 lg:w-7/12 p-2 px-10 leading-loose">
+    <div class="py-4 border-b border-gray-200">
+      <p class="text-2xl text-gray-700 font-semibold">Sera</p>
+      <p>Women Black Printed Elasticated Waist Dress with Tassel Tie-Up</p>
+    </div>
+    <div class="text-gray-700 font-semibold py-3 tracking-wider">
+      <span class="text-2xl mr-2">Rs. 549</span>
+      <span class="font-hairline line-through text-lg mr-2">Rs. 1099</span>
+      <span class="text-orange-500 text-xl">(50% OFF)</span>
+      <p class="text-sm font-hairline">Additional tax shall apply, charged at checkout</p>
+    </div>
+    <div>
+      <div>
+        <span class="font-extrabold text-lg">SELECT SIZE</span>
+        <a href="#" class="font-extrabold text-sm text-pink-500 ml-8">
+          SIZE CHART
+          <i class="fa fa-angle-right ml-2" aria-hidden="true"></i>
+        </a>
+      </div>
+      <div class="flex py-6">
+        <button
+          class="mr-3 rounded-full bg-white border border-gray-400 w-12 h-12 hover:border-black hover:font-bold"
+        >
+          <p>S</p>
+        </button>
+        <button
+          class="mr-3 rounded-full bg-white border border-gray-400 w-12 h-12 hover:border-black hover:font-bold"
+        >
+          <p>M</p>
+        </button>
+        <button
+          class="mr-3 rounded-full bg-white border border-gray-400 w-12 h-12 hover:border-black hover:font-bold"
+        >
+          <p>L</p>
+        </button>
+        <button
+          class="mr-3 rounded-full bg-white border border-gray-400 w-12 h-12 hover:border-black hover:font-bold"
+        >
+          <p>XL</p>
+        </button>
+      </div>
+      <div>
+        <button class="mr-3 bg-red-600 hover:bg-red-500 text-white py-2 px-12 rounded font-bold">
+          <i class="fa fa-shopping-bag mr-2" aria-hidden="true"></i> ADD TO BAG
+        </button>
+        <button class="mr-3 bg-white border border-grey-300 text-black py-2 px-8 rounded font-bold">
+          <i class="fa fa-bookmark mr-2" aria-hidden="true"></i> WISHLIST
+        </button>
+      </div>
+      <div class="py-8 border-b border-gray-300">
+        <p class="font-bold text-lg">
+          BEST OFFERS
+          <i class="fa fa-tag ml-2" aria-hidden="true"></i>
+        </p>
+        <span class="text-gray-500">This product is already at its best price</span>
+        <div>
+          <button
+            class="my-2 w-3/5 relative text-left px-3 py-2 rounded border hover:border-gray-500 focus:outline-none"
+          >
+            <div class="font-bold">10% instant discount on Federal Bank Cards</div>
+            <div
+              class="font-hairline text-gray-500 text-sm"
+            >This product is already at its best price</div>
+            <span class="absolute right-0 top-0 mt-2 mr-3 hover:block invisible">
+              view
+              <i class="fa fa-angle-right ml-2" aria-hidden="true"></i>
+            </span>
+          </button>
+
+          <button
+            class="my-2 w-3/5 relative text-left px-3 py-2 rounded border hover:border-gray-500 focus:outline-none"
+          >
+            <div class="font-bold">10 super cashback on MobiWiki</div>
+            <div class="font-hairline text-gray-500 text-sm">Max super cashback of Rs.250. TCA</div>
+            <span class="absolute right-0 top-0 mt-2 mr-3 hover:block invisible">
+              view
+              <i class="fa fa-angle-right ml-2" aria-hidden="true"></i>
+            </span>
+          </button>
+
+          <button
+            class="my-2 w-3/5 relative text-left px-3 py-2 rounded border hover:border-gray-500 focus:outline-none"
+          >
+            <div class="font-bold">Flat 200 cashback on Airtel Payments Bank</div>
+            <div
+              class="font-hairline text-gray-500 text-sm"
+            >Min spend 2,000;for first time transaction. TCA</div>
+            <span class="absolute right-0 top-0 mt-2 mr-3 hover:block invisible">
+              view
+              <i class="fa fa-angle-right ml-2" aria-hidden="true"></i>
+            </span>
+          </button>
+
+          <button
+            class="my-2 w-3/5 relative text-left px-3 py-2 rounded border hover:border-gray-500 focus:outline-none"
+          >
+            <div class="font-bold">EMI option available</div>
+            <div class="font-hairline text-gray-500 text-sm">EMI starting from Rs.26/month</div>
+            <span class="absolute right-0 top-0 mt-2 mr-3 hover:block invisible">
+              view
+              <i class="fa fa-angle-right ml-2" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
+      </div>
+      <div class="py-8 border-b border-gray-300">
+        <p class="font-bold text-lg">
+          PRODUCT DETAILS
+          <i class="fa fa-list-alt ml-2" aria-hidden="true"></i>
+        </p>
+        <span
+          class="text-gray-500"
+        >Black printed woven fit and flare dress, has a tie-up neck, three-quarter sleeves, flared hem</span>
+        <p class="font-bold text-lg">Size & Fit</p>
+        <span class="text-gray-700">he model (height 5'8") is wearing a size S</span>
+
+        <p class="font-bold text-lg">Material & Care</p>
+        <span class="text-gray-700">
+          Polyester
+          <br />Machine-wash
+        </span>
+
+        <p class="font-bold text-lg">Specifications</p>
+        <div>
+          <div class="w-full flex py-1">
+            <div class="w-1/2 border-b border-gray-400 mr-4">
+              <p class="text-xs text-gray-500">Shape</p>
+              <span class="text-gray-700 text-sm">Fit and Flare</span>
+            </div>
+            <div class="w-1/2 border-b border-gray-400 mr-4">
+              <p class="text-xs text-gray-500">Neck</p>
+              <span class="text-gray-700 text-sm">Tie-Up Neck</span>
+            </div>
+          </div>
+          <div class="w-full flex py-4">
+            <div class="w-1/2 border-b border-gray-400 mr-4">
+              <p class="text-xs text-gray-500">Shape</p>
+              <span class="text-gray-700 text-sm">Fit and Flare</span>
+            </div>
+            <div class="w-1/2 border-b border-gray-400 mr-4">
+              <p class="text-xs text-gray-500">Neck</p>
+              <span class="text-gray-700 text-sm">Tie-Up Neck</span>
+            </div>
+          </div>
+          <div class="w-full flex py-4">
+            <div class="w-1/2 border-b border-gray-400 mr-4">
+              <p class="text-xs text-gray-500">Shape</p>
+              <span class="text-gray-700 text-sm">Fit and Flare</span>
+            </div>
+            <div class="w-1/2 border-b border-gray-400 mr-4">
+              <p class="text-xs text-gray-500">Neck</p>
+              <span class="text-gray-700 text-sm">Tie-Up Neck</span>
+            </div>
+          </div>
+          <div class="w-full flex py-4">
+            <div class="w-1/2 border-b border-gray-400 mr-4">
+              <p class="text-xs text-gray-500">Shape</p>
+              <span class="text-gray-700 text-sm">Fit and Flare</span>
+            </div>
+            <div class="w-1/2 border-b border-gray-400 mr-4">
+              <p class="text-xs text-gray-500">Neck</p>
+              <span class="text-gray-700 text-sm">Tie-Up Neck</span>
+            </div>
+          </div>
+          <span class="font-bold text-pink-500">See More</span>
+        </div>
+      </div>
+      <div class="py-4">
+        <p class="font-bold text-lg">
+          DELIVERY OPTIONS
+          <i class="fa fa-truck ml-2" aria-hidden="true"></i>
+        </p>
+        <div class="text-gray-600 leading-loose">
+          <div class="w-1/3 flex flex-wrap hr-line justify-between pb-2 relative">
+            <input
+              type="search"
+              class="bg-gray-200 border p-2 w-full rounded"
+              placeholder="PinCode"
+            />
+            <button
+              class="absolute right-0 text-sm mt-2 text-pink-500 pr-2 focus:outline-none hover:text-gray-600 cursor-pointer"
+            >Check</button>
+          </div>
+          <span
+            class="text-gray-500 text-xs"
+          >Please enter PIN code to check delivery time & Cash/Card on Delivery Availability</span>
+          <p>Tax: Applicable tax on the basis of exact location & discount will be charged at the time of checkout</p>
+          <p>100% Original Products</p>
+          <p>Free Delivery on order above Rs. 1199</p>
+          <p>Cash on delivery might be available</p>
+          <p>Easy 30 days returns and exchanges</p>
+          <p>Try & Buy might be available</p>
+
+          <p>
+            Product Code:
+            <span>1923967</span>
+          </p>
+
+          <p>
+            Sold by:
+            <a href="#">SERA GROUP</a>
+          </p>
+
+          <p class="font-semibold text-sm text-gray-700">Manufacturer/Packer/Importer Details</p>
+          <span class="text-xs">Sera Group, 1st Floor, Gehlot Farms, Sector-47, Gurgaon</span>
+
+          <p class="font-semibold text-sm text-gray-700">Country of origin</p>
+          <span class="text-xs">India</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
