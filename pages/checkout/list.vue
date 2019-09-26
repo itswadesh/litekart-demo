97365f86627433ebc36a3078b7ee4b2c3a3dfd5d
Kintali Naveesh <coolnavesh@gmail.com>
2019-09-26 15:28:23 +0530

    mobile address completed

--- pages/checkout/list.vue
+++ pages/checkout/list.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="bg-gray-300">
+  <div>
     <CheckoutHeader />
-    <div class="text-sm font-hairline text-gray-600 p-5">CHOOSE DELIVERY ADDRESS</div>
+    <div class="bg-gray-100">
+      <div class="text-sm font-hairline text-gray-600 p-5">CHOOSE DELIVERY ADDRESS</div>
+      <div class="m-2">
+        <div class="shadow bg-white p-2 rounded flex justify-between">
+          <div>check</div>
+          <div>Kintali Naveesh</div>
+          <div class="text-right">OFFICE</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
