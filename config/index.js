e119870527afe4ce5d5779504555fc5e85ae1eef
Swadesh Behera <itswadesh@gmail.com>
2019-09-28 07:23:21 +0530

    timeout 0

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 export const API_URL = 'https://api.litekart.in'
 export const tokenExpiry = 60 * 60 * 15 // Login validity (15 minutes)
-export const typingTimeout = 700 // After this delay the search api will be fired
+export const typingTimeout = 0 // After this delay the search api will be fired
 export const loadingTimeout = 500 // Loading indicator will be shown after this delay
 export const userRoles = ['user', 'vendor', 'manager', 'admin'] // This should be in ascending order of authority. e.g. In this case user will not have access to any other role, where as admin will have the role of user+vendor+manager+admin
 export const currency = { symbol: '₹', code: 'INR' }
