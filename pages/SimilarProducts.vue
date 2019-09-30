2846d8bcfd8256d7ddc3e28f7c1bd095c1fab550
Kintali Naveesh <coolnavesh@gmail.com>
2019-09-30 18:56:30 +0530

    similar products

---
+++ pages/SimilarProducts.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <div class="text-black font-bold text-lg p-2">SIMILAR PRODUCTS</div>
+    <div>
+      <div class="flex flex-wrap justify-between">
+        <a href="#" class="w-1/2 lg:w-56">
+          <div class="shadow m-2 relative">
+            <img class="h-64 w-full object-cover" src="/cart-product.jpg" />
+            <img src="/close-img.png" class="absolute w-4 top-0 right-0 mt-2 mr-2" />
+            <div class="px-2 leading-loose">
+              <div class="text-lg font-bold text-black">Fitbit Inspire HR</div>
+              <div class="text-sm font-hairline text-gray-700">Shirt and bottom jeans</div>
+              <div class="text-gray-500 mb-2 text-xs">
+                <span class="text-lg font-bold text-black mt-2">Rs. 8,999</span>
+                <span class="line-through">₹904</span>
+                <span class>|</span>
+                <span class="text-orange-500 font-hairline">40% OFF</span>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <a href="#" class="w-1/2 lg:w-56">
+          <div class="shadow m-2 relative">
+            <img class="h-64 w-full object-cover" src="/cart-product.jpg" />
+            <img src="/close-img.png" class="absolute w-4 top-0 right-0 mt-2 mr-2" />
+            <div class="px-2 leading-loose">
+              <div class="text-lg font-bold text-black">Fitbit Inspire HR</div>
+              <div class="text-sm font-hairline text-gray-700">Shirt and bottom jeans</div>
+              <div class="text-gray-500 mb-2 text-xs">
+                <span class="text-lg font-bold text-black mt-2">Rs. 8,999</span>
+                <span class="line-through">₹904</span>
+                <span class>|</span>
+                <span class="text-orange-500 font-hairline">40% OFF</span>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <a href="#" class="w-1/2 lg:w-56">
+          <div class="shadow m-2 relative">
+            <img class="h-64 w-full object-cover" src="/cart-product.jpg" />
+            <img src="/close-img.png" class="absolute w-4 top-0 right-0 mt-2 mr-2" />
+            <div class="px-2 leading-loose">
+              <div class="text-lg font-bold text-black">Fitbit Inspire HR</div>
+              <div class="text-sm font-hairline text-gray-700">Shirt and bottom jeans</div>
+              <div class="text-gray-500 mb-2 text-xs">
+                <span class="text-lg font-bold text-black mt-2">Rs. 8,999</span>
+                <span class="line-through">₹904</span>
+                <span class>|</span>
+                <span class="text-orange-500 font-hairline">40% OFF</span>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <a href="#" class="w-1/2 lg:w-56">
+          <div class="shadow m-2 relative">
+            <img class="h-64 w-full object-cover" src="/cart-product.jpg" />
+            <img src="/close-img.png" class="absolute w-4 top-0 right-0 mt-2 mr-2" />
+            <div class="px-2 leading-loose">
+              <div class="text-lg font-bold text-black">Fitbit Inspire HR</div>
+              <div class="text-sm font-hairline text-gray-700">Shirt and bottom jeans</div>
+              <div class="text-gray-500 mb-2 text-xs">
+                <span class="text-lg font-bold text-black mt-2">Rs. 8,999</span>
+                <span class="line-through">₹904</span>
+                <span class>|</span>
+                <span class="text-orange-500 font-hairline">40% OFF</span>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <a href="#" class="w-1/2 lg:w-56">
+          <div class="shadow m-2 relative">
+            <img class="h-64 w-full object-cover" src="/cart-product.jpg" />
+            <img src="/close-img.png" class="absolute w-4 top-0 right-0 mt-2 mr-2" />
+            <div class="px-2 leading-loose">
+              <div class="text-lg font-bold text-black">Fitbit Inspire HR</div>
+              <div class="text-sm font-hairline text-gray-700">Shirt and bottom jeans</div>
+              <div class="text-gray-500 mb-2 text-xs">
+                <span class="text-lg font-bold text-black mt-2">Rs. 8,999</span>
+                <span class="line-through">₹904</span>
+                <span class>|</span>
+                <span class="text-orange-500 font-hairline">40% OFF</span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div>
+      <ul class="flex justify-center flex-wrap py-5">
+        <li
+          class="w-full lg:flex-1 py-3 py-2 px-3 m-1 text-sm lg:rounded-full pointer border border-gray-500 hover:border-red-600 font-bold lg:text-red-500 text-black font-bold jus"
+        >
+          <a href="#" class="flex justify-between">
+            <div>MORE KURTA SETS BY SOUJANYA</div>
+            <i class="fa fa-chevron-right mt-1" aria-hidden="true"></i>
+          </a>
+        </li>
+
+        <li
+          class="w-full lg:flex-1 py-3 py-2 px-3 m-1 text-sm lg:rounded-full pointer border border-gray-500 hover:border-red-600 font-bold lg:text-red-500 text-black font-bold jus"
+        >
+          <a href="#" class="flex justify-between">
+            <div>MORE KURTA SETS BY lipsoum</div>
+            <i class="fa fa-chevron-right mt-1" aria-hidden="true"></i>
+          </a>
+        </li>
+
+        <li
+          class="w-full lg:flex-1 py-3 py-2 px-3 m-1 text-sm lg:rounded-full pointer border border-gray-500 hover:border-red-600 font-bold lg:text-red-500 text-black font-bold jus"
+        >
+          <a href="#" class="flex justify-between">
+            <div>MORE KURTA SETS BY arvind</div>
+            <i class="fa fa-chevron-right mt-1" aria-hidden="true"></i>
+          </a>
+        </li>
+
+        <li
+          class="w-full lg:flex-1 py-3 py-2 px-3 m-1 text-sm lg:rounded-full pointer border border-gray-500 hover:border-red-600 font-bold lg:text-red-500 text-black font-bold jus"
+        >
+          <a href="#" class="flex justify-between">
+            <div>MORE KURTA</div>
+            <i class="fa fa-chevron-right mt-1" aria-hidden="true"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
