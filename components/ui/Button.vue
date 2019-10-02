d8272cad8a291f3a50bc6d49e31dbda281100c06
itswadesh <itswadesh@gmail.com>
2019-10-02 22:19:53 +0530

    -

--- components/ui/Button.vue
+++ components/ui/Button.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="group">
-    <button
-      v-bind="$attrs"
-      :size="size"
-      class="w-full p-3  text-lg font-bold rounded"
-      :class="{'bg-pink-500 hover:bg-pink-600 text-white':color=='primary','text-black border border-gray-300':color=='none'}"
-      :aria-label="label"
-    >
-      <slot />
-    </button>
-  </div>
+  <button
+    v-bind="$attrs"
+    :size="size"
+    class="w-full p-3  text-lg font-bold rounded"
+    :class="{'bg-pink-500 hover:bg-pink-600 text-white':color=='primary','text-black border border-gray-300':color=='none'}"
+    :aria-label="label"
+  >
+    <slot />
+  </button>
 </template>
 
 <script>
