e1ed3f711eac1bf0d3620506d3b1dda995294d61
Swadesh Behera <itswadesh@gmail.com>
2019-10-03 13:01:14 +0530

    Button click

--- components/ui/Button.vue
+++ components/ui/Button.vue
@@ -4,7 +4,7 @@
     :size="size"
     class="w-full p-3 text-lg font-bold rounded"
     :class="{'bg-pink-500 hover:bg-pink-600 text-white':color=='primary','text-black border border-gray-300':color=='none'}"
-    :aria-label="label"
+    @click="handleClick"
   >
     <slot />
   </button>
@@ -16,6 +16,11 @@ export default {
     icon: { type: String },
     size: { type: String, default: "text" },
     color: { type: String, default: "none" }
+  },
+  methods: {
+    handleClick(e) {
+      this.$emit("click", e);
+    }
   }
 };
 </script>
