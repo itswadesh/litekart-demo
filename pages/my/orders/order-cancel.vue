483ed1b203e2227ad148c5971aef11ead86d1c68
itswadesh <itswadesh@gmail.com>
2019-10-05 21:41:01 +0530

    fix: Mobile and desktop (Return page)

--- pages/my/orders/order-cancel.vue
+++ pages/my/orders/order-cancel.vue
@@ -1,80 +1,78 @@
 <template>
   <div style="background:#f5f5f5">
-    <!-- mobile starts -->
-    <div
-      class="w-full font-semibold text-xs justify-between flex border-b-2 border-gray-300 lg:hidden block"
-    >
+    <div class="w-full font-semibold text-xs justify-between flex border-b-2 border-gray-300 lg:hidden block">
       <div class="flex-1 text-center text-pink-500 border-b border-pink-500 bg-white py-4">Select</div>
       <div class="flex-1 py-4 bg-white">
         <div class="flex justify-between">
           <div>
-            <i class="fa fa-angle-right text-gray-500" aria-hidden="true"></i>
+            <i
+              class="fa fa-angle-right text-gray-500"
+              aria-hidden="true"
+            ></i>
           </div>
           <div class="text-gray-500">Reason</div>
           <div>
-            <i class="fa fa-angle-right text-gray-500" aria-hidden="true"></i>
+            <i
+              class="fa fa-angle-right text-gray-500"
+              aria-hidden="true"
+            ></i>
           </div>
         </div>
       </div>
       <div class="flex-1 text-gray-500 py-4 bg-white text-center">Confirm</div>
     </div>
-    <!-- mobile ends -->
     <div class="w-full lg:w-4/5 m-auto flex lg:border lg:border-gray-300">
-      <div class="flex-1" :class="{'mask':step!==1}">
-        <div
-          class="text-center text-pink-500 border-b border-pink-500 bg-white py-4 hidden lg:block"
-        >Select</div>
-        <div>
-          <div
-            class="text-gray-700 text-sm px-6 bg-white border-b border-gray-300 py-4 font-semibold"
-          >Select items for cancellation</div>
-          <div class="p-5 py-4 flex justify-between bg-white rounded">
-            <div>
-              <Checkbox></Checkbox>
-            </div>
-            <div>
-              <img class="lg:rounded xs:rounded-b-none w-16" src="/cart-product.jpg" />
-            </div>
-            <div class="lg:w-4/5 xs:9/12">
-              <div class="pl-4 font-hairline text-sm text-gray-500">
-                <div class="text-gray-700">Stylish Green Floral Print</div>
-                <div class="flex">
+      <div
+        class="flex-1"
+        :class="{'mask hidden lg:block':step!==1}"
+      >
+        <div class="text-center text-pink-500 border-b border-pink-500 bg-white py-4 ">Select</div>
+        <div class="text-gray-700 text-sm px-6 bg-white border-b border-gray-300 py-4 font-semibold">Select items for cancellation</div>
+        <div class="p-5 py-4 flex justify-between bg-white rounded">
+          <div>
+            <Checkbox></Checkbox>
+          </div>
+          <div>
+            <img
+              class="lg:rounded xs:rounded-b-none w-16"
+              src="/cart-product.jpg"
+            />
+          </div>
+          <div class="lg:w-4/5 xs:9/12">
+            <div class="pl-4 font-hairline text-sm text-gray-500">
+              <div class="text-gray-700">Stylish Green Floral Print</div>
+              <div class="flex">
+                <div>
+                  <span>Size:</span>
+                  3-4 years
+                </div>
+              </div>
+              <div class="flex justify-between text-xs mt-2">
+                <div>
                   <div>
-                    <span>Size:</span>
-                    3-4 years
+                    <div class="text-gray-700 font-semibold">Cancel Qty</div>
+                    <select class="rounded focus:outline-none text-left w-full border border-gray-400">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                      <option>6</option>
+                      <option>7</option>
+                    </select>
                   </div>
                 </div>
-                <div class="flex justify-between text-xs mt-2">
-                  <div>
-                    <div>
-                      <div class="text-gray-700 font-semibold">Cancel Qty</div>
-                      <select
-                        class="rounded focus:outline-none text-left w-full border border-gray-400"
-                      >
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>6</option>
-                        <option>7</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div class="text-gray-700 font-semibold">
-                    Refund
-                    <br />
-                    <div class="font-hairline text-green-500">₹0</div>
-                  </div>
+                <div class="text-gray-700 font-semibold">
+                  Refund
+                  <br />
+                  <div class="font-hairline text-green-500">₹0</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div class="fixed bottom-0 relative border-t-2 border-gray-300 bg-white">
-          <div
-            class="flex justify-between text-black font-semibold text-xs px-2 py-4 bg-white shadow"
-          >
+          <div class="flex justify-between text-black font-semibold text-xs px-2 py-4 bg-white shadow">
             <div>1 item selected</div>
             <div>₹0</div>
           </div>
@@ -86,24 +84,31 @@
           </div>
         </div>
       </div>
-      <div class="flex-1 text-center" :class="{'mask':step!==2}">
+      <div
+        class="lg:block flex-1 text-center"
+        :class="{'mask hidden lg:block':step!==2}"
+      >
         <div class="bg-white">
-          <div class="hidden lg:block">
+          <div class="lg:block">
             <div class="flex justify-between py-4">
               <div>
-                <i class="fa fa-angle-right text-gray-500" aria-hidden="true"></i>
+                <i
+                  class="fa fa-angle-right text-gray-500"
+                  aria-hidden="true"
+                ></i>
               </div>
               <div class="text-gray-500">Reason</div>
               <div>
-                <i class="fa fa-angle-right text-gray-500" aria-hidden="true"></i>
+                <i
+                  class="fa fa-angle-right text-gray-500"
+                  aria-hidden="true"
+                ></i>
               </div>
             </div>
           </div>
         </div>
-        <div :class="{'mask':step!==2}">
-          <div
-            class="text-gray-600 text-sm text-left px-10 border-b border-gray-300 py-3"
-          >Why are you cancelling these items?</div>
+        <div class="border-r border-gray-300">
+          <div class="text-gray-600 text-sm text-left px-10 border-b border-gray-300 py-3">Why are you cancelling these items?</div>
           <div class="text-left">
             <ul class="pl-4 text-xs text-gray-600">
               <li>
@@ -143,19 +148,20 @@
           </div>
         </div>
       </div>
-      <div class="flex-1" :class="{'mask':step!==3}">
-        <div class="text-gray-500 py-4 bg-white text-center hidden lg:block">Confirm</div>
-        <div :class="{'mask':step!==3}">
-          <div
-            class="text-gray-600 text-sm px-6 border-b border-gray-300 py-3"
-          >You are cancelling 1 item</div>
+      <div
+        class=" flex-1"
+        :class="{'mask hidden lg:block':step!==3}"
+      >
+        <div class="text-gray-500 py-4 bg-white text-center ">Confirm</div>
+        <div>
+          <div class="text-gray-600 text-sm px-6 border-b border-gray-300 py-3">You are cancelling 1 item</div>
           <div class="text-gray-600 text-xs mt-2 px-6 border-b border-gray-300 py-3">
             <div class="font-semibold">No refund applicable</div>
             <div>Cash on delivery was the payment method chosen from this order</div>
           </div>
           <div>
             <button
-              @click="confirm()"
+              @click="cancelOrder"
               class="w-full mr-2 primary text-white py-2 px-6 font-bold text-sm"
             >Confirm Cancellation</button>
           </div>
@@ -168,17 +174,15 @@
 <script>
 import { Checkbox, Radio } from "~/components/ui";
 export default {
+  components: { Checkbox, Radio },
   data() {
     return {
       step: 1
     };
   },
   methods: {
-    confirm() {
-      console.log("ddone");
-    }
-  },
-  components: { Checkbox, Radio }
+    cancelOrder() {}
+  }
 };
 </script>
 
