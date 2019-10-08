9908a529938bed27dc6cf3895bae35ac5fbdb400
itswadesh <itswadesh@gmail.com>
2019-10-08 18:36:50 +0530

    skeleton issues

--- components/ProductSkeleton.vue
+++ components/ProductSkeleton.vue
@@ -1,12 +1,6 @@
 <template>
-  <div class="flex justify-between flex-wrap">
-    <div
-      v-for="(p,ix) in 10"
-      :key="ix+'-1'"
-      class="flex flex-col px-2 pb-2 w-1/2 lg:w-1/4 xl:w-1/5"
-    >
-      <div class="w-full h-48 lg:p-2 bg-gray-100"></div>
-      <div class="mt-2 p-1 text-center text-1 bg-gray-100 h-6"></div>
-    </div>
+  <div class="w-full flex flex-col px-2 pb-2 w-1/2 lg:w-1/4 xl:w-1/5">
+    <div class="h-48 lg:p-2 bg-gray-100"></div>
+    <div class="mt-2 p-1 text-center text-1 bg-gray-100 h-6"></div>
   </div>
 </template>
--- components/home/YouMayLike.vue
+++ components/home/YouMayLike.vue
@@ -4,13 +4,20 @@
       <div class="w-full p-3 border-b h-14 flex justify-between">
         <p class="w-1/2 float-left relative headings">
           You may also Like
-          <span
-            class="text-gray-500 text-xs absolute inset-x-0 you"
-          >Based on Your Order</span>
+          <span class="text-gray-500 text-xs absolute inset-x-0 you">Based on Your Order</span>
         </p>
         <button class="primary text-xs right-0 py-1 px-2 rounded">View All</button>
       </div>
-      <carousel
+      <div
+        class="overflow-x-auto flex justify-between"
+        v-if="loading"
+      >
+        <ProductSkeleton
+          v-for="(p,ix) in 5"
+          :key="ix+'-1'"
+        />
+      </div>
+      <!-- <carousel
         :perPageCustom="[[425, 2], [768, 3], [1024, 5]]"
         :paginationEnabled="false"
         :navigationEnabled="true"
@@ -18,7 +25,10 @@
         navigation-prev-label="<img src='/chevron-right.svg'/>"
         class="flex flex-wrap justify-between p-4"
       >
-        <slide v-for="product in products" :key="product._id">
+        <slide
+          v-for="product in products"
+          :key="product._id"
+        >
           <nuxt-link :to="'/'+product.slug+'?id='+product._id">
             <div class="relative">
               <img
@@ -38,13 +48,14 @@
             </div>
           </nuxt-link>
         </slide>
-      </carousel>
+      </carousel> -->
     </div>
   </div>
 </template>
 
 <script>
 import { Carousel, Slide } from "vue-carousel";
+import ProductSkeleton from "~/components/ProductSkeleton";
 
 export default {
   data() {
@@ -52,7 +63,7 @@ export default {
       products: []
     };
   },
-  components: { Carousel, Slide },
+  components: { Carousel, Slide, ProductSkeleton },
   async created() {
     try {
       this.loading = true;
@@ -60,7 +71,7 @@ export default {
       this.products = res.data;
     } catch (e) {
     } finally {
-      this.loading = false;
+      // this.loading = false;
     }
   }
 };
